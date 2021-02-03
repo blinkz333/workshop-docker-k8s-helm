ea76d0fa929cddd5b09706e6cfd91a38596fa57d
blink <threerapon.r@trendvg3.com>
2021-02-03 16:01:02 +0700

    fix constant name

--- backend/sonarqube.js
+++ backend/sonarqube.js
@@ -4,9 +4,9 @@ sonarqubeScanner(
     serverUrl: "http://128.199.226.142:9000",
     token: "c5a79b86ff0baf3de961918740a26d9d77739b71",
     options: {
-      "sonar.projectKey": "somkiat",
-      "sonar.projectName": "SomkiatBackend",
-      "sonar.projectDescription": "Backend",
+      "sonar.projectKey": "blinkz333",
+      "sonar.projectName": "blinkz333Backend",
+      "sonar.projectDescription": "blinkz333Backend",
       "sonar.sources": ".",
       "sonar.tests": "src",
       "sonar.inclusions": "**",
